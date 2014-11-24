3b2b209d125c798ee755b5da2f52ca94f3c1030f
Zamith <zamith.28@gmail.com>
2014-11-24 15:01:05 +0000

    27 november presentation

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'http://rubygems.org'
 gem 'activesupport', '~> 3.2'
 gem 'pygments.rb', '~> 0.5'
 gem 'io-console', '~> 0.3' if RUBY_VERSION < '1.9.3'
-
+gem 'rainbow'
---
+++ slides.rb
@@ -0,0 +1,93 @@
+# encoding: utf-8
+
+fade_out_color = "333333"
+
+center <<-EOS
+  #{ " A NEW DAWN FOR MINHO.RB ".inverse }
+
+  #{b "Luis Zamith".upcase }
+  @zamith
+EOS
+
+heading "Needs to be more #{ " ENGAGING ".inverse}"
+
+block <<-EOS
+ 1. #{ " MORE ".inverse } coding, #{ " LESS ".inverse } talking.
+
+ #{ "2. A place to have fun and meet interesting people".color(fade_out_color) }
+
+ #{ "3. You learn and discuss the topics that interest #{ b "you" }".color(fade_out_color) }
+EOS
+
+block <<-EOS
+ #{ "1. #{ " MORE ".inverse }".color(fade_out_color) } #{ "coding, #{ " LESS ".inverse }".color(fade_out_color) } #{ "talking".color(fade_out_color) }
+
+ 2. A place to have fun and meet interesting people
+
+ #{ "3. You learn and discuss the topics that interest #{ b "you" }".color(fade_out_color) }
+EOS
+
+block <<-EOS
+ #{ "1. #{ " MORE ".inverse }".color(fade_out_color) } #{ "coding, #{ " LESS ".inverse }".color(fade_out_color) } #{ "talking".color(fade_out_color) }
+
+ #{ "2. A place to have fun and meet interesting people".color(fade_out_color) }
+
+ 3. You learn and discuss the topics that interest #{ b "you" }
+EOS
+
+diagram <<-EOS
+                 ___                  ____                  ___
+                (   \\              .-'    `-.              /   )____
+           (____     \\_____       /  (O  O)  \\       _____/     ____)
+          (____            `-----(      )     )-----'            ____)
+           (____     __________   \\  -____-  /   __________     ____)
+             (______/          \\   `-.____.-'   /          \\_____)
+                                \\              /
+
+                            Embrace everyone's opinion
+EOS
+
+heading "Occasional guest speaker is still welcome"
+
+heading "More activity in the #{ "interwebs".bright }"
+
+center <<-EOS
+mailing list
+
+twitter
+
+facebook
+
+blog
+
+IRC
+
+...
+EOS
+
+center <<-EOS
+That's my view of the future,
+
+#{ " what's yours? ".upcase.inverse }
+EOS
+
+heading "27th November, 2014"
+
+heading b("Show & Tell Hacknight")
+
+block <<-EOS
+#{ "Beginners:".bright }
+
+Exercism.io first exercise (http://exercism.io/)
+
+Codewars simple exercise (http://codewars.com/kata/fizzbuzz-array-custom)
+
+
+
+
+#{ "Beginners++:".bright }
+
+Codewars exercise (http://www.codewars.com/kata/can-you-get-the-loop)
+
+Your own project (not as fun)
+EOS
